ce0f99e69684fd6934cfff3d9353b522e2366514
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2021-11-06 15:47:11 -0400

    Updated dependencies.

--- go.mod
+++ go.mod
@@ -2,12 +2,10 @@ module github.com/kohirens/iam-user-key-rotator
 
 go 1.17
 
-require (
-	github.com/aws/aws-sdk-go-v2 v1.11.0
-	github.com/aws/aws-sdk-go-v2/config v1.10.0
-)
+require github.com/aws/aws-sdk-go-v2/config v1.10.0
 
 require (
+	github.com/aws/aws-sdk-go-v2 v1.11.0 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.6.0 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.8.0 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.0 // indirect
--- main.go
+++ main.go
@@ -1,15 +1,28 @@
 package main
 
 import(
-	"github.com/aws/aws-sdk-go-v2/aws"
+	"context"
+	"fmt"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"log"
+	"os"
 )
 
 func main() {
+	var mainErr error
+
+	defer func() {
+		if mainErr != nil {
+			log.Fatal(mainErr)
+		}
+		os.Exit(0)
+	}()
+
 	//1. Make a new AWS config
 	// Load the Shared AWS Configuration (~/.aws/config)
 	cfg, err := config.LoadDefaultConfig(context.TODO())
 	if err != nil {
-		log.Fatal(err)
+		mainErr = fmt.Errorf("could not get AWS configuration with default methods; %v", err.Error())
+		return
 	}
 }
