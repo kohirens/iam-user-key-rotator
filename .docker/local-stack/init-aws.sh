93795bf23cfcb96434781e4fbde8c0cbe4a0eb2d
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2021-12-01 01:18:12 -0500

    add: Policy to test user.

--- .docker/local-stack/init-aws.sh
+++ .docker/local-stack/init-aws.sh
@@ -1,2 +1,3 @@
 awslocal iam create-user --user-name auto-roto
 awslocal iam create-access-key --user-name auto-roto > /test-tmp/access-key-secret-auto-roto.json
+awslocal iam put-user-policy --user-name auto-roto --policy-name YourPolicyName --policy-document file:///auto-roto-policy.json
