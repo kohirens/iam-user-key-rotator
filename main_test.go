f2b6e96da2ed205d9975687e7a978dd6228c37f8
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2021-12-12 10:31:54 -0500

    chg: Refactor code to get IAM keys meta data.

--- main_test.go
+++ main_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/iam/types"
 	"io"
 	"log"
 	"net/http"
@@ -20,9 +21,24 @@ const (
 	fixturesDir = "testdata"
 	testTmp     = "tmp"
 	// subCmdFlags space separated list of command line flags.
-	subCmdFlags = "RECURSIVE_TEST_FLAGS"
-	dirMode     = 0700
+	subCmdFlags        = "RECURSIVE_TEST_FLAGS"
+	dirMode            = 0700
+	localStackEndpoint = "http://localstack:4566"
+	localStackGood     = "{\"status\": \"running\"}"
 )
+
+func isLocalStackAvailable() bool {
+	res, err := http.Get(localStackEndpoint)
+	if err == nil {
+		b, e := io.ReadAll(res.Body)
+		if (e == nil || e == io.EOF) && string(b) == localStackGood {
+			log.Println("using localstack for AWS endpoint")
+			return true
+		}
+	}
+
+	return false
+}
 
 func TestMain(m *testing.M) {
 	// Only runs when this environment variable is set. Allows testing the main program in a sub process.
@@ -50,7 +66,7 @@ func TestMain(m *testing.M) {
 			}
 		}
 		// mock HTTP client to spy and assert.
-		hClient = &mockHttpClient{0}
+		httpComm = &mockHttpClient{0}
 		runAppMain()
 	}
 
@@ -84,6 +100,11 @@ func TestDaysOld(tester *testing.T) {
 }
 
 func TestFlags(tester *testing.T) {
+	if !isLocalStackAvailable() {
+		tester.Skip("unknown environment, skipping")
+	}
+
+	// Should only run in a test environment, or it could change your real IAM keys.
 	var tests = []struct {
 		name     string
 		wantCode int
@@ -107,6 +128,47 @@ func TestFlags(tester *testing.T) {
 
 			if got != test.wantCode {
 				t.Errorf("unexpected error on exit. want %q, got %q", test.wantCode, got)
+			}
+		})
+	}
+}
+
+func TestGetIamKeyStats(tester *testing.T) {
+	if !isLocalStackAvailable() {
+		tester.Skip("unknown environment, skipping")
+	}
+	s1 := "ABC123"
+	t1 := time.Date(2021, 12, 15, 1, 0, 0, 0, time.UTC)
+	s2 := "ABCD1234"
+	t2 := time.Date(2021, 12, 10, 1, 0, 0, 0, time.UTC)
+
+	// Should only run in a test environment, or it could change your real IAM keys.
+	var tests = []struct {
+		name        string
+		keys        []types.AccessKeyMetadata
+		daysAllowed int
+		currentId   string
+		wantOld     int
+		wantValid   int
+	}{
+		{"1_valid", []types.AccessKeyMetadata{{AccessKeyId: &s1, CreateDate: &t1}}, 1, s1, 0, 1},
+		{"1_old", []types.AccessKeyMetadata{{AccessKeyId: &s2, CreateDate: &t2}}, 1, s2, 1, 0},
+	}
+
+	for _, test := range tests {
+		tester.Run(test.name, func(t *testing.T) {
+			got := getIamKeyStats(test.keys, test.daysAllowed, *test.keys[0].AccessKeyId)
+
+			if got.current != test.currentId {
+				t.Errorf("current ids do now match. want %q, got %q", test.currentId, got.current)
+			}
+
+			if len(got.old) != test.wantOld {
+				t.Errorf("old array not what expected; want %q, got %q", test.wantOld, len(got.old))
+			}
+
+			if len(got.valid) != test.wantValid {
+				t.Errorf("valid array not what expected; want %q, got %q", test.wantValid, len(got.valid))
 			}
 		})
 	}
